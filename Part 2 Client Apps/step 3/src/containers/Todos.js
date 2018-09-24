0ddfad8180ccc98361133c696f1f6fe95ace4e78
malikumar001 <umawtips1@gmail.com>
2018-09-24 16:33:25 +0500

    writing tests with jest and write component code to pass these tests

---
+++ Part 2 Client Apps/step 3/src/containers/Todos.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+class Todos extends Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Todos;
---
+++ Part 2 Client Apps/step 3/src/containers/__tests__/Todos.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todos from '../Todos.js';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Todos />, div);
+  ReactDOM.unmountComponentAtNode(div);
+
+});
