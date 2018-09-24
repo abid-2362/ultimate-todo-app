95aaa02658ab5356591ad6070735fae328d0e153
malikumar001 <umawtips1@gmail.com>
2018-09-24 16:37:39 +0500

    Integrating enzyme with jest and write test for Todos.js

--- Part 2 Client Apps/step 3/src/containers/__tests__/Todos.test.js
+++ Part 2 Client Apps/step 3/src/containers/__tests__/Todos.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Todos from '../Todos.js';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Todos />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    let mountedTodo = shallow(<Todos />);
 
 });
